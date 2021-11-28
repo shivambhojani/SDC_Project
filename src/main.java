b3317c7a98bef1aa4a08b319431eecb380d52d46
shivambhojani <shivambhojani@gmail.com>
2021-11-28 15:26:25 -0400

    DB connection with local

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -5,13 +6,38 @@ import java.util.UUID;
 public class main {
     public static void main(String[] args) {
 
-        UUID uuid= UUID.randomUUID();
-        System.out.println(uuid);
+        Connection connect = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
 
-        PersonIdentity p = new PersonIdentity();
-        Date thisDate = new Date();
-        SimpleDateFormat dateFormt = new SimpleDateFormat("MMMM dd Y");
-        System.out.println(dateFormt.format(thisDate));
-        PersonIdentity p1 = new PersonIdentity();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //jdbc:mysql://<hostname>
+            connect = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306", "root", "root");
+
+            statement = connect.createStatement();
+            System.out.println("Connection Successful");
+            statement.execute("use project;");
+            resultSet = statement.executeQuery("select name from person limit 3;");
+            while (resultSet.next()) {
+                System.out.println("Person name: " + resultSet.getString("name"));
+            }
+            resultSet.close();
+            statement.close();
+            connect.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+
+//        UUID uuid= UUID.randomUUID();
+//        System.out.println(uuid);
+//
+//        PersonIdentity p = new PersonIdentity();
+//        Date thisDate = new Date();
+//        SimpleDateFormat dateFormt = new SimpleDateFormat("MMMM dd Y");
+//        System.out.println(dateFormt.format(thisDate));
+//        PersonIdentity p1 = new PersonIdentity();
     }
 }
